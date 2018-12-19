32c523dcde056b472fb78282fe01ddb4f332049d
Viktor Pankraz <info@pankraz.org>
2018-12-19 23:41:16 +0100

    reduce memory space for rules from 4k to 2k to have more flash available for code

--- Xmega/ApplicationTable.h
+++ Xmega/ApplicationTable.h
@@ -16,26 +16,26 @@ class ApplicationTable
 {
    public:
 
-    #ifndef APPTABLE_SECTION_ADDR
-    #define APPTABLE_SECTION_ADDR APPTABLE_SECTION_START
+    #ifndef _RULES_OFFSET_
+    #define _RULES_OFFSET_ 0
     #endif
 
       ////    Operations    ////
 
       inline static uint8_t read( uint16_t offset )
       {
-         Flash::address_t address = APPTABLE_SECTION_ADDR + offset;
+         Flash::address_t address = APPTABLE_SECTION_START + _RULES_OFFSET_ + offset;
          return Flash::read( address );
       }
 
       inline static uint16_t read( uint16_t offset, void* pData, uint16_t length )
       {
-         return Flash::read( APPTABLE_SECTION_ADDR + offset, pData, length );
+         return Flash::read( APPTABLE_SECTION_START + _RULES_OFFSET_ + offset, pData, length );
       }
 
       inline static uint16_t write( uint16_t offset, void* pData, uint16_t length )
       {
-         Flash::address_t address = APPTABLE_SECTION_ADDR + offset;
+         Flash::address_t address = APPTABLE_SECTION_START + _RULES_OFFSET_ + offset;
          return Flash::write( address, pData, length );
       }
 };
